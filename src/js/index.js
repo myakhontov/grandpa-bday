1a21a80aa15e3efa033a0e0d5c33a3db1ed9b928
myakhontov <michael.yakhontov23@sitechhs.com>
2020-05-08 04:28:08 -0400

    finished intro animation

--- src/js/index.js
+++ src/js/index.js
@@ -3,9 +3,11 @@ const website = ()=> {
     const start = ()=> {
         const present = document.querySelector('.present')
         const birthdayMessage = document.querySelector('.birthday')
+        const presentCap = document.querySelector('.present-cap')
 
         present.addEventListener('click', ()=>{
             birthdayMessage.classList.add('birthday-pop')
+            presentCap.classList.add('present-open')
         })
     }
 
