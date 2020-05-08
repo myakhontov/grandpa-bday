001476aa0de42fa0c67f0385ebeda5a0d357e578
myakhontov <michael.yakhontov23@sitechhs.com>
2020-05-08 14:43:23 -0400

    added arrow + anim

--- src/js/index.js
+++ src/js/index.js
@@ -4,10 +4,12 @@ const website = ()=> {
         const present = document.querySelector('.present')
         const birthdayMessage = document.querySelector('.birthday')
         const presentCap = document.querySelector('.present-cap')
+        const arrow = document.querySelector('.arrow')
 
         present.addEventListener('click', ()=>{
             birthdayMessage.classList.add('birthday-pop')
             presentCap.classList.add('present-open')
+            arrow.classList.add('fadeIn')
         })
     }
 
