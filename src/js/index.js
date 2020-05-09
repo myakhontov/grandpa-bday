e3174882c4134291bd3aef5505cdfc7fc5815fe5
myakhontov <michael.yakhontov23@sitechhs.com>
2020-05-08 20:37:02 -0400

    slide scroll animation

--- src/js/index.js
+++ src/js/index.js
@@ -13,18 +13,52 @@ const website = ()=> {
         })
     }
     const slides = ()=>{
-        const title = document.querySelector('.title')
         const leftSlides = document.querySelectorAll('.left')
         const leftArr = Array.from(leftSlides)
         const rightSlides = document.querySelectorAll('.right')
         const rightArr = Array.from(rightSlides)
-        console.log(rightArr)
+        
+        const scrFuncLeftOne = ()=>{
+            const y = window.scrollY
+            const distToTop = leftArr[0].getBoundingClientRect().top
+            if (y >= distToTop){
+                leftArr[0].classList.add('left-slide')
+            }else{
+                leftArr[0].classList.remove('left-slide')
+            }
+        }
+        const scrFuncLeftTwo = ()=>{
+            const y = window.scrollY
+            const distToTop = leftArr[1].getBoundingClientRect().top
+            if (y >= distToTop){
+                leftArr[1].classList.add('left-slide')
+            }else{
+                leftArr[1].classList.remove('left-slide')
+            }
+        }
+        const scrFuncRightOne = ()=>{
+            const y = window.scrollY
+            const distToTop = rightArr[0].getBoundingClientRect().top
+            if (y >= distToTop){
+                rightArr[0].classList.add('right-slide')
+            }else{
+                rightArr[0].classList.remove('right-slide')
+            }
+        }
+        const scrFuncRightTwo = ()=>{
+            const y = window.scrollY
+            const distToTop = rightArr[1].getBoundingClientRect().top
+            if (y >= distToTop){
+                rightArr[1].classList.add('right-slide')
+            }else{
+                rightArr[1].classList.remove('right-slide')
+            }
+        }
 
-
-        title.addEventListener('click', ()=>{
-            leftArr.forEach((slide)=> slide.classList.add('slide'))
-            rightArr.forEach((slide)=> slide.classList.add('slide'))
-        } )
+        window.addEventListener('scroll', scrFuncLeftOne)
+        window.addEventListener('scroll', scrFuncLeftTwo)
+        window.addEventListener('scroll', scrFuncRightOne)
+        window.addEventListener('scroll', scrFuncRightTwo)
     }
 
 
@@ -34,3 +68,5 @@ const website = ()=> {
 } 
 //start web func
 website()
+//const x = document.querySelector('.slides')
+//console.log(x.offsetTop)
